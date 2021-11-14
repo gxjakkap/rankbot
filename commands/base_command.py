a5b461454e4ed3ab5afb5b8464abb1ff013c5eff
Jakkaphat <gunt@guntxjakka.me>
2021-11-14 17:41:01 +0700

    add README.md to gitignore, clean base_command.py

--- commands/base_command.py
+++ commands/base_command.py
@@ -1,7 +1,5 @@
 import settings
 
-# Base command class
-# Do not modify!
 class BaseCommand:
 
     def __init__(self, description, params):
@@ -16,6 +14,5 @@ class BaseCommand:
         desc += f": {description}."
         self.description = desc
 
-    # Every command must override this method
     async def handle(self, params, message, client):
-        raise NotImplementedError  # To be defined by every command
+        raise NotImplementedError
