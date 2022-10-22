29e56b102c78e8c17eb8a0ccbaa34e056bdb1744
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-10-23 03:50:06 +0700

    fix: catch unranked val player

--- messages/valrankRes.js
+++ messages/valrankRes.js
@@ -26,12 +26,28 @@ exports.valrankMessage = async (name, tag, region) => {
 
     const res = await getPlayerRank(name, tag, region)
     if (!res.data || res.status===404){
-        return [false, `API returns 404. Either ${name}#${tag} is unranked or simply doesn't exist.`]
+        return [false, `API returns 404. Either ${name}#${tag} is **unranked** or simply doesn't exist.`]
     }
 
     const rankPoint = res.data.ranking_in_tier
     const rankName = res.data.currenttierpatched
     const cdnUrl = getValRankImgLink(res.data.currenttier)
+
+    if (!rankPoint || !rankName){
+        const fields = [
+            {name: "Name", value: `${res.data.name}#${res.data.tag}`},
+            {name: "Region", value: region.toUpperCase()},
+            {name: "Rank", value: `Unranked`},
+        ]
+        const msg = new EmbedBuilder()
+            .setColor(getValRankColor(0))
+            .setAuthor({name: "VALORANT Competitive"})
+            .setImage(getValRankImgLink(0)) 
+            .setFooter({text: "Data provided by henrikdev.xyz"})
+            .addFields(fields)
+        return [true, msg]
+    }
+        
     const fields = [
         {name: "Name", value: `${res.data.name}#${res.data.tag}`},
         {name: "Region", value: region.toUpperCase()},
