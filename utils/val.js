777650019ea9d6608513fcf939f3ea9fea8f4699
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-10-23 04:14:30 +0700

    fix: not being able to have more than 1 space in riot name

--- utils/val.js
+++ utils/val.js
@@ -1,7 +1,19 @@
 exports.processRiotTags = (input) => {
     let inputString = input.toString()
-    if (!inputString.includes("#") || ((inputString.match(/,/g) || []).length > 1)){return null}
+
+    if (!inputString.includes("#") /* || ((inputString.match(/,/g) || []).length > 1) */){
+        return null
+    }
+
     let rtagArr = inputString.split("#")
-    if (rtagArr.length > 2){return null}
+
+    if (rtagArr.length > 2){ // if there's more than one #
+        return null
+    }
+
+    if ((rtagArr[1].length > 5) || (rtagArr[0].length > 16)){ // riot id's limits name length at 16 characters and tag length at 5 characters
+        return null
+    }
+
     return {"name": rtagArr[0], "tag": rtagArr[1]}
 }
