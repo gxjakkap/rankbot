ec03af9d8c734cbdc6bc915e9f768ca8e64e2250
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2023-10-16 03:36:42 +0700

    chore: remove rookie image condition

--- utils/img.js
+++ utils/img.js
@@ -7,11 +7,6 @@ const getValRankImgLink = (tier) => {
 const getApexRankImgLink = (rankName, rankDiv) => {
     const x = `${rankName.toLowerCase()}${rankDiv.toString()}`
     let y = x.replace(" ", "")
-
-    if (y.includes("rookie")){ // all rookie rank will use the same asset file
-        y = "rookie4"
-    }
-
     return `${apexImgCdnBaseUrl}/${y}.png`
 }
 
